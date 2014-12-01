4e182f0db11533790f7e7e3e429c835e47966918
Vlad Velici <vlad.velici@gmail.com>
2014-12-01 20:56:39 +0000

    Nicer message for fromEprintsCsv

--- eigen/fromEprintsCsv.m
+++ eigen/fromEprintsCsv.m
@@ -4,7 +4,7 @@ function [ adj ] = fromEprintsCsv( path )
     % force symmetric graph;
     unq = unique(data(:));
     data = [data(:,1) data(:,2); data(:,2) data(:,1)];
-    display(size(unq));
+    disp('Read data with unique nodes:');
+    disp(size(unq,1));
     adj = sparse(data(:,1),data(:,2),ones(size(data,1),1),size(unq,1),size(unq,1));
 end
-
