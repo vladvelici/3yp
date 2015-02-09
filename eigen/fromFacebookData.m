df6c877392cea3dbca924cb8267bd55085a89780
Vlad Velici <vlad.velici@gmail.com>
2015-02-09 21:53:40 +0000

    Reading facebook data

---
+++ eigen/fromFacebookData.m
@@ -0,0 +1,8 @@
+function [ adj ] = fromFacebookData( path )
+    raw = csvread(path);
+    
+    % friends is a symmetric relationship
+    raw = [raw(:,1), raw(:,2); raw(:,2), raw(:,1)];
+    
+    adj = sparse(raw(:,1), raw(:,2), ones(size(raw,1),1));
+end
