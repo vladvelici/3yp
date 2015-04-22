40e15ea55411be4fd8a3df13fe39bce1378125a3
Vlad Velici <vlad.velici@gmail.com>
2015-04-22 21:45:21 +0100

    Heuristics comments and removed debug print()

--- py/heuristics.py
+++ py/heuristics.py
@@ -1,5 +1,4 @@
-# Heuristics implementations using sparse graphs here.
-# Using heuristics needs the original adjacency matrix stored.
+# Heuristics implementations are found in this package.
 
 import networkx as nx
 
@@ -7,6 +6,9 @@ class Maxdepth:
     """Heuristics are totally decopuled from the main algorithm.
 
     Using and linking them together is, however, trivial and even elegant.
+
+    This is the Maximum Depth Heuristic. It uses networkx for the graph
+    algorithms.
     """
     def __init__(self,edgelist, depth):
         self.g = nx.DiGraph()
@@ -15,14 +17,17 @@ class Maxdepth:
         self._pairs = None
 
     def top(self,nodes=None):
-        """Yields (from,to) pairs. Arguments can be:
-        - no arguments: Apply heuristic for all graph.
-        - a node: Applies heuristic on that node only.
-        - a list of nodes: Applies heuristic on all nodes of the list."""
+        """The first time this function is run, the heuristic is computed
+        for the whole graph and result is saved.
+
+        If nodes==Node or a list
+            - it returns a dict of form {from_node: [list_of_to_nodes]}
+        Otherwise,
+            - it returns a list of _to_ nodes.
+        """
 
         if self._pairs is None:
-            print("Computing HEU")
-            p = nx.all_pairs_shortest_path_length(self.g, self.depth) 
+            p = nx.all_pairs_shortest_path_length(self.g, self.depth)
             self._pairs = {k: [i for i,_ in tos.items()] for k, tos in p.items()}
 
         if nodes is None:
@@ -32,6 +37,8 @@ class Maxdepth:
         return {k: self._pairs[k] for k in nodes }
 
     def topGen(self, nodes=None):
+        """It uses top(), but instead of returning the result, it is a generator
+        function that (always) yields pairs of nodes."""
         res = self.top(nodes)
 
         if isinstance(res, list):
@@ -42,4 +49,3 @@ class Maxdepth:
             for i,l in res.items():
                 for j in l:
                     yield((i,j))
-
